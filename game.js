3729ce44d03756801daffa4da47a95fa578b52f7
unknown <jenatcanil14@gmail.com>
2024-10-11 17:29:09 +0530

    sound and effects

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
 buttonColors= ["red","blue","green","yellow"]
 gamePattern=[]
+$(document).keydown(function() {
+    nextSequence();
+});
 function nextSequence(){
     var num=Math.floor(Math.random()*4);
     var randomColor=buttonColors[num]
     gamePattern.push(randomColor)
+    console.log(randomColor)
+    $(document).find("#"+randomColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var sound = new Audio('./sounds/' + randomColor + '.mp3');
+    sound.play();
 }
-nextSequence()
